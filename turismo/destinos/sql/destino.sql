24cf2a605360c73c5297f8c2b55f30533f9383c8
Irapoam Dias <irapoam.dias@gmail.com>
2014-07-20 11:31:56 -0300

    Adicionando script para inserir destinos automaticamente

---
+++ turismo/destinos/sql/destino.sql
@@ -0,0 +1,5 @@
+INSERT INTO destinos_destino VALUES (1,"Destino 1");
+INSERT INTO destinos_destino VALUES (2,"Destino 2");
+INSERT INTO destinos_destino VALUES (3,"Destino 3");
+INSERT INTO destinos_destino VALUES (4,"Destino 4");
+INSERT INTO destinos_destino VALUES (5,"Destino 5");
