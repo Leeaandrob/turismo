a37752fcc69eb158a8430aa2f8f2e24b4c847fdf
Irapoam Dias <irapoam.dias@gmail.com>
2014-07-20 11:24:30 -0300

    Continuacao do modulo de grupos

--- turismo/grupos/models.py
+++ turismo/grupos/models.py
@@ -4,10 +4,17 @@ from turismo.destinos.models import Destino
 # Create your models here.
 
 
+SITUACAO = (
+        ('A', 'Em Aberto'),
+        ('B', 'Concluído'),
+        ('C', 'Cancelado'),
+        )
+
 class Grupo(models.Model):
     name = models.CharField(max_length=100)
     destino = models.ForeignKey(Destino)
     partida = models.CharField(max_length=10)
     chegada = models.CharField(max_length=10)
     numpessoas = models.CharField(max_length=3)
+    situacao = models.CharField(max_length=1,choices=SITUACAO)
 
