e232cfb007008500aff3c5e86df3c5858d060dd0
Irapoam Dias <irapoam.dias@gmail.com>
2014-08-19 01:39:06 -0300

    app core

---
+++ turismo/core/views.py
@@ -0,0 +1,7 @@
+#coding:utf-8
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.core.urlresolvers import reverse as r
+
+def home(request):
+	return render(request, 'base.html')
